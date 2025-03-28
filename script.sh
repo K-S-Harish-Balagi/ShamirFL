a521888d82c96aa345305835a2749ca7a11a306a
K-S-Harish-Balagi <85448207+K-S-Harish-Balagi@users.noreply.github.com>
2025-03-28 16:47:43 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ sudo apt update -y
 sudo apt install -y python3-pip
 
 echo "Installing required Python packages..."
-pip install --break-system-packages numpy pandas scikit-learn tensorflow websockets phe ShamirSecret
+pip install --break-system-packages numpy pandas websockets phe 
 
 echo "Updating PATH for locally installed binaries..."
 echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
